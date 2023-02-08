6f4d9b23bde178c41d15b0f6929481f4ab4cf209
Faiz Ali <fali@bsd220.org>
2023-02-08 09:40:36 -0600

    Initial Commit

---
+++ Map HackWich/Map_HackWichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Map_HackWichApp.swift
+//  Map HackWich
+//
+//  Created by Faiz Ali on 2/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Map_HackWichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
